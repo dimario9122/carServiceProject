23c8787dbd3d183f59068f7a973f42ca786ce481
akaspat <akaspat@mail.ru>
2020-11-10 14:45:12 +0300

    #1 add hello function

--- hello.py
+++ hello.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return 'Hello World'
 
+@app.route('/hello')
+def say_hi_natali():
+    return 'Hello Natali!'
+
+@app.route('/hi')
+def say_hi_dime():
+    return 'Hello Dima'
+
 if __name__ == "__main__":
     app.run()
