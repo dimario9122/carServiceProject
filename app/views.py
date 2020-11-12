866d94a138f818337f694b846b651020ccee5401
akaspat <akaspat@mail.ru>
2020-11-12 21:34:19 +0300

    #2 получение пароля для логина из формы

--- app/views.py
+++ app/views.py
@@ -1,4 +1,6 @@
 from flask import render_template, url_for, redirect, request, jsonify
+import sqlite3
+import os
 from app import app
 
 
@@ -17,8 +19,28 @@ def login():
     if request.method == 'GET':
         return render_template('login.html')
     if request.method == 'POST':
+        # получение данных из запроса
         payload = {
             'username': request.form.get('username'),
             'password': request.form.get('password')
         }
-        return jsonify(payload)
+
+        #
+        # запрос пароля из базы данных
+        #
+
+        # получение абсолютного пути к файлу базы данных
+        path_to_db_file = os.path.abspath('.')
+        path_to_db_file += '/app/database.db'
+        connect_db = sqlite3.connect(path_to_db_file)
+        cursor = connect_db.cursor()
+
+        # запрос на получение пароля для логина из формы
+        query = "SELECT Password FROM client WHERE Login='%s'" % payload['username']
+        cursor.execute(query)
+        data_from_db = cursor.fetchall()
+
+        return jsonify({
+            'form_data': payload,
+            'database_data': data_from_db
+        })
