a35f7c7aaf238727ec06c051b9224ed271f71f2f
akaspat <akaspat@mail.ru>
2020-11-12 22:14:42 +0300

    #2 standart login_passwd checking

--- app/views.py
+++ app/views.py
@@ -25,22 +25,37 @@ def login():
             'password': request.form.get('password')
         }
 
-        #
-        # запрос пароля из базы данных
-        #
-
+        '''
+        запрос пароля из базы данных
+        '''
         # получение абсолютного пути к файлу базы данных
         path_to_db_file = os.path.abspath('.')
         path_to_db_file += '/app/database.db'
         connect_db = sqlite3.connect(path_to_db_file)
         cursor = connect_db.cursor()
 
-        # запрос на получение пароля для логина из формы
+        # получение пароля для текущего логина из формы
         query = "SELECT Password FROM client WHERE Login='%s'" % payload['username']
         cursor.execute(query)
-        data_from_db = cursor.fetchall()
+        data_from_db = cursor.fetchone()
 
-        return jsonify({
-            'form_data': payload,
-            'database_data': data_from_db
-        })
+        connect_db.close()
+        '''
+        запрос пароля из базы данных выполнен
+        '''
+
+        # если пользователя с такми логином нет в бд
+        if data_from_db is None:
+            message = 'Неверный логин или пароль'
+            return render_template('login.html', message=message)
+
+        # проверка на необходимость смены стандартного пароля
+        if data_from_db[0] == payload['password'] == 'admin' and payload['username'] == 'admin':
+            return 'Redirect to page change passwd'
+
+        # проверка пользовательского пароля и пароля из бд
+        if data_from_db[0] == payload['password']:
+            return 'Redirect to home page. Welcome user'
+        else:
+            message = 'Неверный логин или пароль'
+            return render_template('login.html', message=message)
