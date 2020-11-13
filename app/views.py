a5a6cba2ad7a178df5082172d101202187c34f35
akaspat <akaspat@mail.ru>
2020-11-13 17:24:07 +0300

    #3

--- app/views.py
+++ app/views.py
@@ -17,7 +17,7 @@ def home():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
-        return render_template('login.html')
+        return render_template('login_manager.html')
     if request.method == 'POST':
         # получение данных из запроса
         payload = {
@@ -35,7 +35,7 @@ def login():
         cursor = connect_db.cursor()
 
         # получение пароля для текущего логина из формы
-        query = "SELECT Password FROM client WHERE Login='%s'" % payload['username']
+        query = "SELECT Password FROM client WHERE Login='{0}'".format(payload['username'])
         cursor.execute(query)
         data_from_db = cursor.fetchone()
 
@@ -47,15 +47,46 @@ def login():
         # если пользователя с такми логином нет в бд
         if data_from_db is None:
             message = 'Неверный логин или пароль'
-            return render_template('login.html', message=message)
+            return render_template('login_manager.html', message=message)
 
         # проверка на необходимость смены стандартного пароля
         if data_from_db[0] == payload['password'] == 'admin' and payload['username'] == 'admin':
-            return 'Redirect to page change passwd'
+            return redirect(url_for('change_password', username=payload['username']))
 
         # проверка пользовательского пароля и пароля из бд
         if data_from_db[0] == payload['password']:
-            return 'Redirect to home page. Welcome user'
+            return redirect(url_for('home'))
         else:
             message = 'Неверный логин или пароль'
-            return render_template('login.html', message=message)
+            return render_template('login_manager.html', message=message)
+
+
+@app.route('/change_password', methods=['GET', 'POST'])
+def change_password():
+    username = ''
+    status = 'Требуется изменение стандартного пароля!'
+    if request.method == 'GET':
+        username = request.args.get('username')
+    if request.method == 'POST':
+        status = 'Пароль успешно изменен!'
+        username = request.form.get('username')
+        form_passwd = request.form.get('password')
+        '''
+        запись нового пароля в бд
+        '''
+        # получение абсолютного пути к файлу базы данных
+        path_to_db_file = os.path.abspath('.')
+        path_to_db_file += '/app/database.db'
+        connect_db = sqlite3.connect(path_to_db_file)
+        cursor = connect_db.cursor()
+
+        # получение пароля для текущего логина из формы
+        query = "UPDATE client SET Password='{0}' WHERE Login='{1}'".format(form_passwd, username)
+        cursor.execute(query)
+        connect_db.commit()
+
+        connect_db.close()
+        '''
+        конец записи нового пароля в бд
+        '''
+    return render_template('change_password.html', username=username, status=status)
